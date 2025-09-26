672c095c089439dc5e497eeac2ee18fee49a43d5
C_Quark <e.currihuil01@ufromail.cl>
2025-09-25 22:07:43 -0300

    feat: add ChildCalendarScreen and ChildMainScreen with navigation

---
+++ lib/features/child/calendar/child_calendar_screen.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+
+class ChildCalendarScreen extends StatelessWidget {
+  const ChildCalendarScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
---
+++ lib/features/child/child_main_screen.dart
@@ -0,0 +1,40 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_hooks/flutter_hooks.dart';
+import 'package:tora_frontend/features/child/calendar/child_calendar_screen.dart';
+
+class ChildMainScreen extends HookWidget {
+  const ChildMainScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    final selectedIndex = useState(0);
+    final List<Widget> pages = [
+      const ChildCalendarScreen(), // CalendarScreen(),
+      const Placeholder(), // RecommendationsScreen(),
+      const Placeholder(), // PetScreen(),
+      const Placeholder(), // CommunicationScreen(),
+    ];
+
+    return Scaffold(
+      body: pages[selectedIndex.value],
+      bottomNavigationBar: BottomNavigationBar(
+        items: const [
+          BottomNavigationBarItem(icon: Icon(Icons.home), label: 'Calendario'),
+          BottomNavigationBarItem(
+            icon: Icon(Icons.search),
+            label: 'Recomendaciones',
+          ),
+          BottomNavigationBarItem(icon: Icon(Icons.person), label: 'Mascota'),
+          BottomNavigationBarItem(
+            icon: Icon(Icons.settings),
+            label: 'Comunicación',
+          ),
+        ],
+        currentIndex: selectedIndex.value,
+        onTap: (index) {
+          selectedIndex.value = index;
+        },
+      ),
+    );
+  }
+}
